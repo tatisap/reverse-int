2a8a6dcd49d6bc8db18822cf5e7cf73091df63a6
Tatiana Sapun <tatiana.sapun@gmail.com>
2022-01-12 15:35:26 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let processedNumber;
+    (n < 0) ? processedNumber = -n : processedNumber = n;
+
+    let reversedProcessedNumber = String(processedNumber).split('').reverse().join('');
+
+    return Number(reversedProcessedNumber);
 }
